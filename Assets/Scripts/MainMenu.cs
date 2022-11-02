6c5e931b0c3db1394f5d6ad4fddde813a5f26d7a
jkeka <64746435+jkeka@users.noreply.github.com>
2022-11-02 08:15:02 +0200

    Menu added

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public GameObject QuitPanel;
+
+    void Awake()
+    {
+        QuitPanel.SetActive(false);
+    }
+
+    public void GameScene()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void TutorialScene()
+    {
+        SceneManager.LoadScene("TutorialScene");
+        //Debug.Log("Launch tutorial scene");
+    }
+
+    public void ExitGame()
+    {
+        QuitPanel.SetActive(true);
+    }
+
+    public void YesButton()
+    {
+        Application.Quit();
+    }
+
+    public void NoButton()
+    {
+        QuitPanel.SetActive(false);
+    }
+
+}
