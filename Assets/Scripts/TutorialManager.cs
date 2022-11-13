dff588893c236b5ffc5f0b80c625c4d74cdf9776
jkeka <64746435+jkeka@users.noreply.github.com>
2022-11-13 23:47:11 +0200

    Tutorial developed further

--- Assets/Scripts/TutorialManager.cs
+++ Assets/Scripts/TutorialManager.cs
@@ -14,15 +14,18 @@ public class TutorialManager : MonoBehaviour
 
     public GameObject bolts;
     public GameObject tutMotorDummy;
+    public GameObject tutMotorStartDummy;
     public GameObject tutMotorSocket;
     public GameObject tutMotor;
     public GameObject impactWrench;
+    public GameObject impactWrenchDummy;
     public GameObject screen;
     public GameObject arrow;
 
     public GameObject rightController;
     public GameObject teleController;
 
+    public GameObject turnProvider;
 
     public GameObject leftStick;
     public GameObject rightStick;
@@ -32,6 +35,7 @@ public class TutorialManager : MonoBehaviour
     public Transform tutMotorStartPosition;
 
     public bool isMotorLifted;
+    public bool accomplishedPlayed;
 
     public int tutStageInt;
     public int attachedBolts;
@@ -57,7 +61,7 @@ public class TutorialManager : MonoBehaviour
         bolts.SetActive(false);
         tutMotorDummy.SetActive(false);
         tutMotorSocket.SetActive(false);
-        tutMotor.SetActive(true);
+        //tutMotor.SetActive(true);
 
         infoPanel_Panel.SetActive(true);
 
@@ -67,7 +71,11 @@ public class TutorialManager : MonoBehaviour
         rightController.SetActive(false);
         teleController.SetActive(false);
 
+        turnProvider.SetActive(false);
+
         isMotorLifted = false;
+        accomplishedPlayed = false;
+
     }
 
     void Update()
@@ -97,6 +105,8 @@ public class TutorialManager : MonoBehaviour
 
         if (tutStageInt == 1)
         {
+            turnProvider.SetActive(true);
+
             leftStick.SetActive(false);
             rightStick.SetActive(true);
             rightController.SetActive(true);
@@ -111,11 +121,14 @@ public class TutorialManager : MonoBehaviour
         if (tutStageInt == 2)
         {
             rightStick.SetActive(false);
+            tutMotorStartDummy.SetActive(false);
+            tutMotor.SetActive(true);
+
             tutMotor.GetComponent<Outline>().enabled = true;
             arrow.SetActive(true);
             infoText.text = "Pick up the motor by pressing grip";
             toDoPanelScript.doneImagesList[tutStageInt].SetActive(true);
-
+            tutMotor.SetActive(true);
 
         }
 
@@ -137,6 +150,8 @@ public class TutorialManager : MonoBehaviour
             tutMotor.GetComponent<Outline>().enabled = false;
 
             tutMotorSocket.GetComponent<Outline>().enabled = false;
+            impactWrenchDummy.SetActive(false);
+            impactWrench.SetActive(true);
             impactWrench.GetComponent<Outline>().enabled = true;
             infoText.text = "Pick up the impact wrench";
             toDoPanelScript.doneImagesList[tutStageInt].SetActive(true);
@@ -205,9 +220,22 @@ public class TutorialManager : MonoBehaviour
         audioSource.PlayOneShot(granted, 1f);
     }
 
+    public void PlayGrantedTele()
+    {
+        if (tutStageInt == 1)
+        {
+            audioSource.PlayOneShot(granted, 1f);
+
+        }
+    }
+
     public void PlayAccomplished()
     {
-        audioSource.PlayOneShot(accomplished, 1f);
+        if (accomplishedPlayed == false)
+        {
+            audioSource.PlayOneShot(accomplished, 1f);
+            accomplishedPlayed = true;
+        }
     }
 
  
