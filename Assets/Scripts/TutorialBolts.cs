7bd43e3d78e623dc55f33c0eb73d01e4a178d42b
jkeka <64746435+jkeka@users.noreply.github.com>
2022-11-13 15:38:44 +0200

    Tutorial scene

---
+++ Assets/Scripts/TutorialBolts.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class TutorialBolts : MonoBehaviour
+{
+    public Material M_Int_Hover;
+    public Material M_Floor;
+
+    public GameManager gameManager;
+    public TutorialManager tutorialManager;
+
+
+    public InputActionReference toggleReference = null;
+
+    bool isOnPerimeter = false;
+
+    void Awake()
+    {
+
+        toggleReference.action.started += Toggle;
+
+        gameObject.GetComponent<Renderer>().material = M_Floor;
+
+    }
+
+
+    private void OnDestroy()
+    {
+
+        toggleReference.action.started -= Toggle;
+
+    }
+
+
+
+    private void OnTriggerEnter(Collider other) //Toiminnallisuus, kun pelaaja menee sis��n 
+    {
+
+        if (other.tag == "Wrench" && gameManager.stageInt == 1)
+        {
+            gameManager.allBoltsFloorColor = false;
+            gameObject.GetComponent<Renderer>().material = M_Int_Hover;
+            isOnPerimeter = true;
+        }
+
+    }
+    private void OnTriggerExit(Collider other)
+    {
+
+        if (other.tag == "Wrench" && tutorialManager.tutStageInt == 1)
+        {
+
+            gameObject.GetComponent<Renderer>().material = M_Floor;
+            isOnPerimeter = false;
+        }
+
+
+    }
+
+    private void Toggle(InputAction.CallbackContext context)
+    {
+        if (isOnPerimeter == true && tutorialManager.tutStageInt == 1)
+        {
+            bool isActive = !gameObject.activeSelf;
+            AttachBolt();
+            Debug.Log("Bolt clicked with wrench");
+        }
+
+    }
+
+    public void AttachBolt()
+    {
+        //gameManager.allBoltsLoose = false;
+        Destroy(GetComponent<Outline>());
+
+        gameObject.GetComponent<Renderer>().material = M_Floor;
+        gameManager.PlayGranted();
+        tutorialManager.attachedBolts++;
+        Debug.Log("Bolt attached");
+
+        Destroy(this);
+
+
+    }
+}
--- Assets/Scripts/TutorialManager.cs
+++ Assets/Scripts/TutorialManager.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+
 
 public class TutorialManager : MonoBehaviour
 {
@@ -8,19 +10,148 @@ public class TutorialManager : MonoBehaviour
     public AudioClip accomplished;
     AudioSource audioSource;
 
-    public int stageInt;
+    public List<GameObject> boltsList;
+
+    public GameObject bolts;
+    public GameObject tutMotorDummy;
+    public GameObject tutMotorSocket;
+    public GameObject tutMotor;
+    public GameObject impactWrench;
+    public GameObject screen;
+
+    public GameObject leftStick;
+    public GameObject rightStick;
+
+
+
+    public int tutStageInt;
     public int attachedBolts;
 
+    //public InfoPanel infoPanel;
+
+    public ToDoPanel toDoPanelScript;
+
+   // public GameObject toDoPanel;
+    public GameObject infoPanel_Panel;
 
 
-    void Start()
+
+    public TMPro.TMP_Text taskText;
+    public TMPro.TMP_Text infoText;
+
+
+
+    void Awake()
     {
-        
+        tutStageInt = 0;
+        bolts.SetActive(false);
+        tutMotorDummy.SetActive(false);
+        tutMotorSocket.SetActive(false);
+
+        infoPanel_Panel.SetActive(true);
+
+        audioSource = GetComponent<AudioSource>();
+
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (tutStageInt == 0)
+        {
+            leftStick.SetActive(true);
+
+        }
+
+        if (tutStageInt == 1)
+        {
+            leftStick.SetActive(false);
+            rightStick.SetActive(true);
+
+            infoText.text = "Press the left trigger forward and press grip to teleport";
+            toDoPanelScript.doneImagesList[tutStageInt].SetActive(true);
+
+
+        }
+
+        if (tutStageInt == 2)
+        {
+            rightStick.SetActive(false);
+            tutMotor.GetComponent<Outline>().enabled = true;
+
+            infoText.text = "Pick up the motor by pressing grip";
+            toDoPanelScript.doneImagesList[tutStageInt].SetActive(true);
+
+
+        }
+
+        if (tutStageInt == 3)
+        {
+            tutMotor.GetComponent<Outline>().enabled = false;
+            tutMotorSocket.GetComponent<Outline>().enabled = true;
+
+            infoText.text = "Set the motor to it's base";
+            toDoPanelScript.doneImagesList[tutStageInt].SetActive(true);
+
+        }
+
+        if (tutStageInt == 4)
+        {
+            tutMotorSocket.SetActive(false);
+            tutMotorDummy.SetActive(true);
+            tutMotorSocket.GetComponent<Outline>().enabled = false;
+            impactWrench.GetComponent<Outline>().enabled = true;
+            infoText.text = "Pick up the impact wrench";
+            toDoPanelScript.doneImagesList[tutStageInt].SetActive(true);
+
+        }
+
+
+        if (tutStageInt == 5)
+        {
+            bolts.SetActive(true);
+            foreach (GameObject bolt in boltsList)
+            {
+                bolt.GetComponent<Outline>().enabled = true;
+            }
+            infoText.text = "Tighten the bolts with impact wrench pressing the trigger";
+            toDoPanelScript.doneImagesList[tutStageInt].SetActive(true);
+
+
+        }
+
+        if (tutStageInt == 6)
+        {
+
+            impactWrench.GetComponent<Outline>().enabled = false;
+            infoText.text = "Tutorial complete! Restart or exit by using the screen";
+            screen.GetComponent<Outline>().enabled = false;
+            toDoPanelScript.doneImagesList[tutStageInt].SetActive(true);
+
+
+        }
+
+
+        if (attachedBolts >= boltsList.Count)
+        {
+            tutStageInt = 6;
+
+        }
+
+    }
+
+    public void TutStageIntTo1()
+    {
+        tutStageInt = 1;
+    }
+
+    public void PlayGranted()
+    {
+        audioSource.PlayOneShot(granted, 1f);
+    }
+
+    public void PlayAccomplished()
+    {
+        audioSource.PlayOneShot(accomplished, 1f);
     }
 }
