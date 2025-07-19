65a0ded5ddd6aa4eab3e14ab4b4e8cd3916e3804
jkeka <64746435+jkeka@users.noreply.github.com>
2025-07-19 11:10:09 +0300

    Cleaning and commenting the code

--- Assets/Scripts/CoverRemovable.cs
+++ Assets/Scripts/CoverRemovable.cs
@@ -3,90 +3,80 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+// Handles the behavior of a removable cover in the game
+// Includes visual feedback, interaction triggers, and stage transitions
+
 public class CoverRemovable : MonoBehaviour
 {
-    
+    // Material for when the cover is highlighted
     public Material M_Int_Hover;
+
+    // Default metal material
     public Material M_GreyMetal;
 
+    // Reference to the GameManager controlling global state
     public GameManager gameManager;
 
-
+    // Rigidbody for enabling/disabling gravity
     public Rigidbody rb;
 
-
+    // Called on Awake. Sets up layer collision rules
     private void Awake()
     {
+        // Initially ignore collisions between layers 7 and 16
         Physics.IgnoreLayerCollision(7, 16, true);
     }
 
+    // Updates visual feedback and physics rules based on game state
     private void Update()
     {
         if (gameManager.stageInt == 4)
         {
+            // Enable outline and allow collisions for pickup
             gameObject.GetComponent<Outline>().enabled = true;
             Physics.IgnoreLayerCollision(7, 16, false);
 
         }
-
         if (gameManager.stageInt == 7)
         {
+            // Re-enable outline for placing the cover back
             gameObject.GetComponent<Outline>().enabled = true;
-
         }
     }
 
-
-
-    private void OnTriggerEnter(Collider other) //Toiminnallisuus, kun pelaaja menee sis��n 
+    // Triggered when another collider enters the cover’s trigger zone
+    // Used for detecting when the cover is placed near the correct socket
+    private void OnTriggerEnter(Collider other)
     {
-        /*/
-        if (other.tag == "GameController" && gameManager.stageInt == 4)
-        {
-            gameObject.GetComponent<Renderer>().material = M_Int_Hover;
 
-
-        }
-        */
         if (other.tag == "CoverRemovableSocket")
         {
             gameManager.coverOnSocket = true;
-
-
         }
-
     }
+
+    // Triggered when another collider exits the cover’s trigger zone
+    // Resets placement state when leaving the socket area
     private void OnTriggerExit(Collider other)
     {
-        /*
-        if (other.tag == "GameController")
-        {
-
-            gameObject.GetComponent<Renderer>().material = M_GreyMetal;
-        }
-        */
         if (other.tag == "CoverRemovableSocket")
         {
             gameManager.coverOnSocket = false;
-
-
         }
-
     }
 
-
-
+    // Called when the player grabs the cover
+    // Disables visual outline
     public void CoverGrabbed()
     {
         gameObject.GetComponent<Outline>().enabled = false;
         Debug.Log("Cover grapped");
-
-
     }
-
+    
+    // Called when the player releases the cover
+    // Advances game stage based on current state and socket placement
     public void CoverAbandoned()
     {
-
         if (gameManager.stageInt == 4)
         {
             gameManager.stageInt = 5;
@@ -101,11 +91,6 @@ public class CoverRemovable : MonoBehaviour
             Debug.Log("Stage int to 8, cover set back");
             gameManager.stageInt = 8;
             gameManager.PlayGranted();
-
         }
-
     }
-
-
-
 }
