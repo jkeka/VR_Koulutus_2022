7c4e72858311d3f06fcf6a7625b469c2eea95f3b
jkeka <64746435+jkeka@users.noreply.github.com>
2025-07-19 12:01:00 +0300

    Cleaning and commenting the code

--- Assets/Scripts/RestartButton.cs
+++ Assets/Scripts/RestartButton.cs
@@ -4,57 +4,58 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.InputSystem;
 
-
+// This script handles the interaction logic for a Restart button in the scene
+// When the player enters the trigger area and activates the input, the game restarts
 public class RestartButton : MonoBehaviour
 {
+    // Materials to show visual feedback when hovered or idle
     public Material M_Int_Hover;
     public Material M_Base;
 
+    // Reference to Game Manager
     public GameManager gameManager;
 
-
+    // Input action used to trigger the button press
     public InputActionReference toggleReference = null;
 
+    // Flag to check if player is within the button's trigger area
     bool isOnPerimeter = false;
 
+    // Called in the beginning
     void Awake()
     {
+        // Subscribe to input action when the script is initialized
         toggleReference.action.started += Toggle;
     }
 
     private void OnDestroy()
     {
+        // Unsubscribe to avoid memory leaks or input issues
         toggleReference.action.started -= Toggle;
-
     }
 
 
-
-    private void OnTriggerEnter(Collider other) //Toiminnallisuus, kun pelaaja menee sis��n 
+    // If the GameController enters the trigger area, change material and allow interaction
+    private void OnTriggerEnter(Collider other)
     {
-
         if (other.tag == "GameController")
         {
             gameObject.GetComponent<Renderer>().material = M_Int_Hover;
             isOnPerimeter = true;
-
-
         }
-
     }
+
+    // If the GameController leaves the trigger area, reset material and disable interaction
     private void OnTriggerExit(Collider other)
     {
-
         if (other.tag == "GameController")
         {
-
             gameObject.GetComponent<Renderer>().material = M_Base;
             isOnPerimeter = false;
         }
-
-
     }
 
+    // If player is within the button area and activates input, trigger the button function
     private void Toggle(InputAction.CallbackContext context)
     {
         if (isOnPerimeter == true)
@@ -62,14 +63,12 @@ public class RestartButton : MonoBehaviour
             bool isActive = !gameObject.activeSelf;
             RestartButtonPush();
         }
-
     }
 
+    // Play confirmation sound and reload the main game scene
     public void RestartButtonPush()
     {
         gameManager.PlayGranted();
         SceneManager.LoadScene("GameScene");
-
     }
-
 }
