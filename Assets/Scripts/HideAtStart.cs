de65456a68f711dcbbe07a3d144c7dd8ffba4231
jkeka <64746435+jkeka@users.noreply.github.com>
2025-07-19 11:34:02 +0300

    Cleaning and commenting the code

--- Assets/Scripts/HideAtStart.cs
+++ Assets/Scripts/HideAtStart.cs
@@ -2,13 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// Disables the GameObject this script is attached immediately when the scene loads
 public class HideAtStart : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // Called once when the script instance is being loaded.
+    // Sets the GameObject inactive to hide it at the start.
     private void Awake()
     {
         gameObject.SetActive(false);
     }
 }
-
-
