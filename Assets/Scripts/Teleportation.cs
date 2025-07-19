a505405d3a0052dfc2f0af7cbdc4db34ac1c847b
jkeka <64746435+jkeka@users.noreply.github.com>
2025-07-19 13:26:57 +0300

    Cleaning and commenting the code

--- Assets/Scripts/Teleportation.cs
+++ Assets/Scripts/Teleportation.cs
@@ -3,27 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-
+// Handles the teleportation system in the game
 public class Teleportation : MonoBehaviour
 {
-
+    // Reference to RightHandTele gameobject
     public GameObject RightHandTele;
+
+    // Reference to RightHandAction gameobject
     public GameObject RightHandAction;
 
-
+    // Input action reference for toggling teleport interaction
     public InputActionReference toggleTeleportReference = null;
 
-
-    void Awake()
-    {
-        //RightHandTele.SetActive(false);
-        //RightHandAction.SetActive(true);
-
-    }
-
-
+    // Called every frame
     void Update()
     {
+        // Reads the current value of the input action as a Vector2
         Vector2 value = toggleTeleportReference.action.ReadValue<Vector2>();
         print("arvo: " + value);
     }
