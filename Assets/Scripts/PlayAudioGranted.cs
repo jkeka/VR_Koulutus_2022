5bfe0debd4d6bafc462d4a6dce71b14b221c6464
jkeka <64746435+jkeka@users.noreply.github.com>
2025-07-19 11:57:40 +0300

    C

--- Assets/Scripts/PlayAudioGranted.cs
+++ Assets/Scripts/PlayAudioGranted.cs
@@ -2,17 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// Script saved just in case
 public class PlayAudioGranted : MonoBehaviour
 {
-
-
-
-    private void Start()
-    {
-    }
-
-
-
-
 
 }
