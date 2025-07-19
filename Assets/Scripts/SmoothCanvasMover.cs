1f35d86b9c56249ff2445799ff38c702267eea0b
jkeka <64746435+jkeka@users.noreply.github.com>
2025-07-19 13:29:28 +0300

    Cleaning and commenting the code

--- Assets/Scripts/SmoothCanvasMover.cs
+++ Assets/Scripts/SmoothCanvasMover.cs
@@ -2,27 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//  Script to move the canvas smoothly to desired target
 public class SmoothCanvasMover : MonoBehaviour
 {
+    // The target transform the canvas will follow
     public Transform target;
-    private float smoothTime = 1.0f;
-    Vector3 velocity = Vector3.zero;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // Time it takes to smooth the movement
+    private float smoothTime = 1.0f;
+
+    // Current velocity, used by SmoothDamp for smooth interpolation
+    Vector3 velocity = Vector3.zero;
 
     // Update is called once per frame
     void Update()
     {
+        // Calculate the desired position relative to the target
         Vector3 targetPosition = target.TransformPoint(new Vector3(0, 5, -10));
 
         // Smoothly move the camera towards that target position
         transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
 
+        // Match the rotation of the target to keep the canvas facing the same way
         transform.eulerAngles = target.eulerAngles;
-
     }
 }
