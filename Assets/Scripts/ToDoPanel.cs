1851ab21d9df2143f65839badaf10a77097fe9eb
jkeka <64746435+jkeka@users.noreply.github.com>
2025-07-19 12:11:36 +0300

    Cleaning and commenting the code

--- Assets/Scripts/ToDoPanel.cs
+++ Assets/Scripts/ToDoPanel.cs
@@ -3,54 +3,52 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-
+// This script manages the To-Do Panel UI in the game.
 public class ToDoPanel : MonoBehaviour
 {
+    // Reference to the info panel UI element
     public GameObject infoPanel;
 
+    // Reference to the GameManager script
     public GameManager gameManager;
 
+    // List of GameObjects representing task completion indicators
     public List<GameObject> doneImagesList;
 
+    // Boolean to track whether the to-do menu is active
     public bool isMenuActive;
 
+    // Input action reference to toggle this panel (e.g., controller button)
     public InputActionReference toggleReference = null;
 
-    
-
+    // Called in beginning
     private void Awake()
     {
-        gameObject.SetActive(false);
+        gameObject.SetActive(false);    // Hide the to-do panel when the scene starts
 
-        isMenuActive = false;
+        isMenuActive = false;    // Initialize the menu active status
+
+        // Subscribe the Toggle function to the input event
         toggleReference.action.started += Toggle;
 
+        // Disable all done image indicators at the start
         foreach (GameObject images in doneImagesList)
         {
             images.SetActive(false);
         }
-
     }
 
-    private void Update()
-    {
-
-    }
-
+    // Unsubscribe the input event when the object is destroyed to prevent memory leaks
     private void OnDestroy()
     {
         toggleReference.action.started -= Toggle;
     }
 
-
-
+    // Called when the toggle input is activated
     private void Toggle(InputAction.CallbackContext context)
     {
-        bool isActive = !gameObject.activeSelf;
+        // Toggle the panel's active state
+        bool isActive = !gameObject.activeSelf;    
         gameObject.SetActive(isActive);
-
-        //infoPanel.SetActive(false);
-
     }
-    
 }
