799d3661b5fd6ef2a28df2cd4c96ee8031489f16
jkeka <64746435+jkeka@users.noreply.github.com>
2025-07-19 11:57:51 +0300

    Cleaning and commenting the code

--- Assets/Scripts/PlayAudioGranted.cs
+++ Assets/Scripts/PlayAudioGranted.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-// Script saved just in case
+// Script saved just in case.
 public class PlayAudioGranted : MonoBehaviour
 {
 
