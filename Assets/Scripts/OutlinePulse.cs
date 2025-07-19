9c7ac22a3c8088f77d3f49a6430a47a27dff18a7
jkeka <64746435+jkeka@users.noreply.github.com>
2025-07-19 11:56:45 +0300

    Cleaning and commenting the code

--- Assets/Scripts/OutlinePulse.cs
+++ Assets/Scripts/OutlinePulse.cs
@@ -2,23 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// This script is intended to create a pulsing effect for an object's outline width
 public class OutlinePulse : MonoBehaviour
 {
-
+    // Public variable to hold the current outline width
     public float outlineWidth;
-
-
-    private void Start()
-    {
-        
-    }
 
     private void Update()
     {
+        // This line currently resets the outlineWidth every frame to a static Lerp result
         outlineWidth = Mathf.Lerp(3f, 7f, 1 * Time.deltaTime);
+
+        // Output the current width to the console (for debugging purposes)
         Debug.Log(outlineWidth);
     }
-
-
-
 }
