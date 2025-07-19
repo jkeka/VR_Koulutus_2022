f2b9983cf2d36c1ab2eb8c7b71896346bd4c05c8
jkeka <64746435+jkeka@users.noreply.github.com>
2025-07-19 11:55:10 +0300

    Cleaning and commenting the code

--- Assets/Scripts/MenuPanel.cs
+++ Assets/Scripts/MenuPanel.cs
@@ -3,45 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-
+// Script not in active use, saved just in case
 public class MenuPanel : MonoBehaviour
 {
     public GameObject infoPanel;
-
-
     public bool isMenuActive;
-
     public InputActionReference toggleReference = null;
-
-    /*
-
-    private void Awake()
-    {
-        gameObject.SetActive(false);
-        isMenuActive = false;
-        toggleReference.action.started += Toggle;
-
-    }
-
-    private void Update()
-    {
-
-    }
-
-    private void OnDestroy()
-    {
-        toggleReference.action.started -= Toggle;
-    }
-
-
-
-    private void Toggle(InputAction.CallbackContext context)
-    {
-        bool isActive = !gameObject.activeSelf;
-        gameObject.SetActive(isActive);
-
-        infoPanel.SetActive(false);
-
-    }
-    */
 }
